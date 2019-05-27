c4da2b86c2dc7818deda183a0810e2a94661e36e
Ryan Gilfillan <regilfillan@gmail.com>
2019-05-27 20:38:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,10 +15,6 @@ class Song
     @@songs << self
   end
 
-  # def self.find_by_name(name)
-  #   @@songs.detect{|a| a.name == name}
-  # end
-
   def self.all
     @@songs
   end
@@ -27,7 +23,4 @@ class Song
     @artist = artist
   end
 
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
 end
