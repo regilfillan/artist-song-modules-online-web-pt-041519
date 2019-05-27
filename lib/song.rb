65297e4f8923eb9c1d37c22040c7835bb7d88671
Ryan Gilfillan <regilfillan@gmail.com>
2019-05-27 20:33:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Song
   
   extend Memorable::ClassMethods
-  include Findable::IntanceMethods
+  include Findable::InstanceMethods
   
   attr_accessor :name
   attr_reader :artist
