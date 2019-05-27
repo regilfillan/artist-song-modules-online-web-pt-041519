df5cdefa0e8886edd1c14cce3457120d54807895
Ryan Gilfillan <regilfillan@gmail.com>
2019-05-27 20:30:16 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,8 +1,8 @@
 module Findable
   module InstanceMethods
     
-    def self.find_by_name(name)
-     @@artists.detect{|a| a.name == name}
+    def find_by_name(name)
+     all.detect{|a| a.name == name}
     end
   
   
